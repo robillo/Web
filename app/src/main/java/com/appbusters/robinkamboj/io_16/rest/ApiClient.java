ba4c419715e4e09478cfaf714df89c61ac49a056
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-01-15 00:24:42 +0530

    ...

--- app/src/main/java/com/appbusters/robinkamboj/io_16/rest/ApiClient.java
+++ app/src/main/java/com/appbusters/robinkamboj/io_16/rest/ApiClient.java
@@ -1,8 +1,25 @@
 package com.appbusters.robinkamboj.io_16.rest;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 /**
  * Created by robinkamboj on 15/01/17.
  */
 
 public class ApiClient {
+
+    public static final String BASE_URL = "http://api.themoviedb.org/3/";
+    private static Retrofit retrofit = null;
+
+
+    public static Retrofit getClient() {
+        if (retrofit==null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
 }
--- app/src/main/java/com/appbusters/robinkamboj/io_16/rest/ApiInterface.java
+++ app/src/main/java/com/appbusters/robinkamboj/io_16/rest/ApiInterface.java
@@ -1,8 +1,20 @@
 package com.appbusters.robinkamboj.io_16.rest;
 
+import com.appbusters.robinkamboj.io_16.model.MoviesResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
 /**
  * Created by robinkamboj on 15/01/17.
  */
 
 public interface ApiInterface {
+    @GET("movie/top_rated")
+    Call<MoviesResponse> getTopRatedMovies(@Query("api_key") String apiKey);
+
+    @GET("movie/{id}")
+    Call<MoviesResponse> getMovieDetails(@Path("id") int id, @Query("api_key") String apiKey);
 }
