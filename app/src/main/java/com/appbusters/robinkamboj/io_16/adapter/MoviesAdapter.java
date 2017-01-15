8c58fbde8798903d39494f4f065c5eeace4fa3ae
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-01-15 14:58:27 +0530

    Movies Adapter

--- app/src/main/java/com/appbusters/robinkamboj/io_16/adapter/MoviesAdapter.java
+++ app/src/main/java/com/appbusters/robinkamboj/io_16/adapter/MoviesAdapter.java
@@ -1,8 +1,69 @@
 package com.appbusters.robinkamboj.io_16.adapter;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.appbusters.robinkamboj.io_16.R;
+import com.appbusters.robinkamboj.io_16.model.Movies;
+
+import java.util.List;
+
 /**
  * Created by robinkamboj on 15/01/17.
  */
 
-public class MoviesAdapter {
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MovieViewHolder> {
+
+    private List<Movies> movies;
+    private int rowLayout;
+    private Context context;
+
+    public MoviesAdapter(List<Movies> movies, int rowLayout, Context context) {
+        this.movies = movies;
+        this.rowLayout = rowLayout;
+        this.context = context;
+    }
+
+    public class MovieViewHolder extends RecyclerView.ViewHolder {
+
+        LinearLayout moviesLayout;
+        TextView movieTitle;
+        TextView data;
+        TextView movieDescription;
+        TextView rating;
+
+
+        public MovieViewHolder(View v) {
+            super(v);
+            moviesLayout = (LinearLayout) v.findViewById(R.id.movies_layout);
+            movieTitle = (TextView) v.findViewById(R.id.title);
+            data = (TextView) v.findViewById(R.id.subtitle);
+            movieDescription = (TextView) v.findViewById(R.id.description);
+            rating = (TextView) v.findViewById(R.id.rating);
+        }
+
+    }
+
+    @Override
+    public MovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(MovieViewHolder holder, final int position) {
+        holder.movieTitle.setText(movies.get(position).getTitle());
+        holder.data.setText(movies.get(position).getReleaseDate());
+        holder.movieDescription.setText(movies.get(position).getOverview());
+        holder.rating.setText(movies.get(position).getVoteAverage().toString());
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
 }
